ec4e30f3e6b88d4fed9185327e42c3b6ab1282cc
Diana Rico <dricom@misena.edu.co>
2021-11-25 05:21:17 -0500

    Modifiqué el título y el encabezado Ejemplo por Empleado

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejemplo</title>
+    <title>Empleado</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel ='stylesheet'  href ='./style.css'/>
 </head>
 <body>
 
 <div class='container-fluid'>
-    <h1>Ejemplo</h1><br/>
+    <h1>Empleado</h1><br/>
     <div class='row'>
     <div class="col-md-6">
      <form method='POST' action ='./FrmMostrar_Estudiante.php'>
